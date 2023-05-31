cb3e1a9e882088e236210f4f8febe48f67f0da1b
narellageorg <130798547+narellageorg@users.noreply.github.com>
2023-05-30 23:47:57 -0300

    Add files via upload

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,5 +1,16 @@
-function AlertIt() {
-var answer = confirm ("Please click on OK to continue.")
-if (answer)
-window.location="index.html";
+
+let edad = prompt("¿Cuál es tu edad?", "22");
+let text;
+if (edad >= 18) {
+  text = "¡Hola, hola! Puedes seguir navegando~";
+} else {
+  text = "¡Ups! Continúa navegando bajo tu propio riesgo.";
 }
+
+		
+
+		window.onbeforeunload = popup;
+
+window.addEventListener("beforeunload", (event) => {
+  event.returnValue = true;
+});
---
+++ javascript/javascript2.js
@@ -0,0 +1,6 @@
+
+
+let locacion = confirm("¿Vivis en Buenos Aires?");
+	if(locacion == true) { 
+			alert("¡Felicidades, no deberás pagar costo de envío por los álbumes de Rocket Punch!")}
+			else{ alert("Cualquier pedido te costará el precio de envío acorde")}	;
